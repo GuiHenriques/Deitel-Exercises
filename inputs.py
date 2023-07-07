69f4c467e3e18aae4e6ba54ef9ffe4cec33b9cb2
GuiHenriques <gui10ghc@gmail.com>
2023-07-07 12:30:28 -0300

    Other files

---
+++ inputs.py
@@ -0,0 +1,5 @@
+# Code to generate data input for C programs via command line arguments
+# for i in range(22, 47):
+#    print(f"ex_{i}.c ", end="")
+for i in range(12,30):
+    print(f"ex_{i}.c ", end="")
